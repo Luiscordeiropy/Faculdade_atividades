ea37433c7d74c24ae58cfc240a35de77bb3f3b8f
Luiscordeiro <luiscordeiro2006@gmail.com>
2024-09-13 15:34:58 -0300

    Atualização

--- Python/ex04.py
+++ Python/ex04.py
@@ -1,20 +1,20 @@
 class Aluno:
-    def __init__(self, nome: str, nota1: float, nota2: float ):
-        self.__nome = nome
-        self.__nota1 = nota1
-        self.__nota2 = nota2
+    def __init__(self, nome, nota1, nota2 ):
+        self.nome = nome
+        self.nota1 = nota1
+        self.nota2 = nota2
 
-    def dizer_nome(self):
-        print(f"Nome: {self.__nome}")
+    def dizer_nome(self, ):
+        print(self.nome)
 
     def dizer_nota1(self):
-        print(f"nota1: {self.__nota1}")
+        print(self.nota1)
 
     def dizer_nota2(self):
-        print(f"Nota2: {self.__nota2}")
-    
+        print(self.nota2)
+
     def dizer_media(self):
-        print((self.__nota1 + self.__nota2)/2)
+        print((self.nota1 + self.nota2)/2)
 
 aluno = Aluno(nome="Luis", nota1 =5, nota2 =7)
 
